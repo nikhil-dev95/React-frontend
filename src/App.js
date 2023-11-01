76ce4930e9440fe0bc04e7064850cf958c8df01b
Nikhil <nikhilbhadouriya2003@gmail.com>
2023-11-01 22:30:39 +0530

    Front-end

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css'
+import Homepage from "./Components/Homepage/homepage"
+import Login from "./Components/Login/login"
+import Register from "./Components/Register/register"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from 'react';
+
+function App() {
+
+  const [ user, setLoginUser] = useState({})
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {
+              user && user._id ? <Homepage setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser}/>
+            }
+          </Route>
+          <Route path="/login">
+            <Login setLoginUser={setLoginUser}/>
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
